f6725739b7a488a41e397467ce12f9b65089dcb8
Abhinav <abhinavponnu964531@gmail.com>
2025-07-23 15:36:17 +0530

    updated uri

--- server/api/server.js
+++ server/api/server.js
@@ -30,4 +30,4 @@ app.use('/api/tickets', exitStationRouter)
 //     console.log(`Server is running on port ${PORT}`);
 // })
 
-module.exports.handler = serverless(app);
+export const handler = serverless(app);
