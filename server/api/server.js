7b1ea744aec24555af5f0a62571bb22f4679f139
Abhinav <abhinavponnu964531@gmail.com>
2025-07-23 15:30:02 +0530

    updated uri

--- server/api/server.js
+++ server/api/server.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors';
-import connectDB from './config/db.js';
-import stations from './routes/stations.js';
-import calculatePrice from './routes/calculateprice.js';
-import bookTicketRouter from './routes/bookticket.js';
-import enterStation from './routes/enterStation.js';
-import exitStationRouter from './routes/exitStation.js';
+import connectDB from '../config/db.js';
+import stations from '../routes/stations.js';
+import calculatePrice from '../routes/calculateprice.js';
+import bookTicketRouter from '../routes/bookticket.js';
+import enterStation from '../routes/enterStation.js';
+import exitStationRouter from '../routes/exitStation.js';
 const app = express();
 app.use(cors());
 app.use(express.json());
